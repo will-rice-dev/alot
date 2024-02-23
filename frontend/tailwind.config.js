a31f5705a57bd286854a60a80f307917d17d77f9
will-rice-dev <will.rice.dev@gmail.com>
2024-02-23 08:12:06 -0500

    Added custom theme and login route

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,21 @@ export default {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ["aqua", "dark"],
+    themes: ["aqua", "dark",
+      {
+        mytheme: {
+          "primary": "#489FB5",
+          "secondary": "#82C0CC",
+          "accent": "#FFA62B",
+          "neutral": "#EDE7E3",
+          "base-100": "#16697A",
+          "info": "#489FB5",
+          "success": "#82C0CC",
+          "warning": "#a25800",
+          "error": "#c50028",
+        }
+      },
+    ]
   }
 }
 
