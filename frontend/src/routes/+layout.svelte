e76b024f791d1b364533991d920301acf060bdae
will-rice-dev <will.rice.dev@gmail.com>
2024-02-29 14:25:39 -0500

    Added navbar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 
 <script>
   import "tailwindcss/tailwind.css";
+  import Navigation from "../components/Navigation.svelte";
+  import { page } from "$app/stores"
 </script>
 
+{#if $page.url.pathname != "/login"}
+  <Navigation />
+{/if}
 <slot />
 
